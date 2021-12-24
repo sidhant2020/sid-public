bfafee57cf487f4fa9606c8439678799c5beb1bc
Sidhant Meru <62112772+sidhant2020@users.noreply.github.com>
2021-12-24 14:59:10 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,20 @@
+package org.sunbeam.dac;
+
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Name	:	");
+		String name = sc.nextLine();
+		
+		System.out.print("Empid	:	");
+		int empid = sc.nextInt();
+		
+		System.out.print("Salary	:	");
+		float salary = sc.nextFloat();
+		
+		System.out.println(name+"	"+empid+"	"+salary);
+	}
+}
